987ddcda3b8e34705ec77ac1ccedf4a185bf4211
FancyChaos <Felixs.Developer@tutanota.com>
2022-08-01 22:00:32 +0200

    Minor changes

--- installations/10-install_yst.sh
+++ installations/10-install_yst.sh
@@ -9,7 +9,7 @@ cd yst/
 mv config.mk.openbsd config.mk
 git apply additional_patches/00-openbsd.diff
 # Uncomment for bigger font size
-git apply additional_patches/10-fontsize28.diff
+# git apply additional_patches/10-fontsize28.diff
 
 make
 doas make install
