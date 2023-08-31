de06398ef1e5d10d08aba816fc52b9dbad609c08
FancyChaos <Felixs.Developer@tutanota.com>
2023-08-31 21:55:00 +0200

    Add more completion plugins

--- rootfs/home/.config/nvim/lua/plugins.lua
+++ rootfs/home/.config/nvim/lua/plugins.lua
@@ -31,15 +31,20 @@ require('lazy').setup({
     -- Autocompletion
     'hrsh7th/nvim-cmp',
     dependencies = {
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+
       -- Snippet Engine & its associated nvim-cmp source
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
 
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-
       -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
+      'rafamadriz/friendly-snippets'
+
     },
   },
 
